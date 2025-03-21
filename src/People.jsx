0e71c90ae47e3b0e473651f0a6107ccfb32b98fe
kawsar121 <tohidulislamkawsarbhuiyan2567gmail.com>
2025-03-21 12:08:36 +0600

    css add

--- src/People.jsx
+++ src/People.jsx
@@ -1,8 +1,10 @@
+import './people.css'
+
 export default function People({manush}){
     // destructure korbo
     const {name,email} = manush; //Simple way
     return(
-        <div>
+        <div className="pe">
             {/* <p>Name : {manush.name}</p> #evave kora jabe but simple way ase */}
             <p>Name: {name}</p>
             <p>Email: {email}</p>
